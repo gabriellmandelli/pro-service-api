939af5a192d1d8796d4025d65647654930e65a37
gabriellmandelli <gabriel-mandelli@hotmail.com>
2020-09-19 17:17:32 -0300

    Removido Optional e adicionado busca de ordem de serviço por ID de funcionário;

--- src/main/java/com/proService/maintanance/rules/domain/repository/ManutencaoRespository.java
+++ src/main/java/com/proService/maintanance/rules/domain/repository/ManutencaoRespository.java
@@ -6,11 +6,10 @@ import com.proService.maintanance.rules.domain.enums.SituacaoManutencao;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ManutencaoRespository extends BaseRepository<Manutencao> {
-    Optional<List<Manutencao>> findByFuncionarioId(UUID funcionarioId);
-    Optional<List<Manutencao>> findByFuncionarioIdAndSituacao(UUID funcionarioId, SituacaoManutencao situacaoManutencao);
+    List<Manutencao> findByFuncionarioId(UUID funcionarioId);
+    List<Manutencao> findByFuncionarioIdAndSituacao(UUID funcionarioId, SituacaoManutencao situacaoManutencao);
 }
--- src/main/java/com/proService/maintanance/rules/domain/repository/OrdemServicoRespository.java
+++ src/main/java/com/proService/maintanance/rules/domain/repository/OrdemServicoRespository.java
@@ -5,10 +5,10 @@ import com.proService.maintanance.rules.domain.entity.OrdemServico;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrdemServicoRespository extends BaseRepository<OrdemServico> {
-    Optional<List<OrdemServico>> findByManutencaoId(UUID id);
+    List<OrdemServico> findByManutencaoId(UUID id);
+    List<OrdemServico> findByManutencaoFuncionarioIdOrderByDataOcorrencia(UUID id);
 }
